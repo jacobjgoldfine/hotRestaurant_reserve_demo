7bca66305dc05f6980319ff27320043ca1662981
Jacob Goldfine <jacobjgoldfine@gmail.com>
2021-07-15 21:26:35 -0500

    some minor fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Initialization
 //      Imports
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 //      Express Config
 const app = express();
@@ -15,36 +15,35 @@ let waitList = []; // Overflow container for reservations
 
 // Routes
 //      Get Homepage
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './dist/index.html')));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./dist/index.html")));
 
 //      Get Tables
-app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, './dist/table.html')));
+app.get("/tables", (req, res) => res.sendFile(path.join(__dirname, "./dist/table.html")));
 
 //      Get Reserve
-app.get('/reservation', (req, res) => res.sendFile(path.join(__dirname, './dist/reserve.html')));
+app.get("/reservation", (req, res) => res.sendFile(path.join(__dirname, "./dist/reserve.html")));
 
 //      API Routes
 //              Tables
-app.get('/api/tables', (req, res) => res.json(tables));
+app.get("/api/tables", (req, res) => res.json(tables));
 
 //              Waitlist
-app.get('/api/waitlist', (req, res) => res.json(waitList));
+app.get("/api/waitlist", (req, res) => res.json(waitList));
 
 //      Post to Tables
-app.post('/api/reserve', (req, res) => {
+app.post("/api/reserve", (req, res) => {
+  const newReservation = req.body;
 
-    const newReservation = req.body;
-  
-    if (tables.length < 5) {
-        tables.push(newReservation);
-        res.status(200);
-    } else {
-        waitList.push(newReservation);
-        res.status(201);
-    }
-    console.log(tables);
-    res.json(newReservation);
-  });
+  if (tables.length < 5) {
+    tables.push(newReservation);
+    res.status(200);
+  } else {
+    waitList.push(newReservation);
+    res.status(201);
+  }
+  console.log(tables);
+  res.json(newReservation);
+});
 
 // Listen
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
